a49348af1fd1b94d947d110c5dd08cf638a7cb16
manie.coetzee <katGriep0921>
2017-11-12 10:19:45 +0200

    updated readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import {AmLoggerModule, LogConfig, LogLevel} from "@bi8/am-logger";
+import {AmStorageModule} from "./modules/am-storage/am-storage.module";
 
+const logConfig : LogConfig = {
+  level: LogLevel.debug
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmLoggerModule,
+    AmStorageModule
   ],
-  providers: [],
+  providers: [{
+    provide: 'LogConfig',
+    useValue: logConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
