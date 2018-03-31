91323b38eca417b8baa141ff2f176f5150015064
manie.coetzee <katGriep0921>
2018-03-31 10:47:13 +0200

    fix for ng-packagr complaining about an error

--- public_api.ts
+++
@@ -1,2 +0,0 @@
-export { AmStorageModule } from './src/app/modules/am-storage/am-storage.module'
-export { StorageService } from './src/app/modules/am-storage/storage.service'
---
+++ src/public_api.ts
@@ -0,0 +1,2 @@
+export { AmStorageModule } from './app/modules/am-storage/am-storage.module'
+export { StorageService } from './app/modules/am-storage/storage.service'
